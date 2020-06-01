206ff06d49a47431aec07e8ee57824af79a48c8b
Cedric Buchmuller <cedric.buchmuller@etudiant.univ-lr.fr>
2020-06-01 15:12:45 +0200

    ajout de l'endroit pour confirmer l'initialisation du projet chez tout le monde

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,16 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        // Ajoutez un commentaire ci-dessous !
+        // cbuchmul: ok!
+        //
+        //
+        //
+        //
+        //
+        //
+
     }
 
 
