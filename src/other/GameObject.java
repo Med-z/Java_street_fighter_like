a0cbadc8a846eb5d01ed8a0c382fa8718816b96d
Cedric Buchmuller <cedric.buchmuller@etudiant.univ-lr.fr>
2020-06-02 11:45:36 +0200

    +interface Renderable, +maj gameobject (protected attributs)

--- src/other/GameObject.java
+++ src/other/GameObject.java
@@ -3,7 +3,7 @@ package other;
 import javafx.scene.Node;
 
 public abstract class GameObject {
-    double x, y, width, height;
+    protected double x, y, width, height;
 
     public GameObject(double x, double y, double width, double height) {
         this.x = x;
