ad41026e149c0cbee4fee0e07fcf7d265e788321
Cedric Buchmuller <cedric.buchmuller@etudiant.univ-lr.fr>
2020-06-02 11:51:09 +0200

    +classe Character

---
+++ src/Character.java
@@ -0,0 +1,28 @@
+import interfaces.Renderable;
+import javafx.scene.shape.Rectangle;
+import other.GameObject;
+import other.Hitbox;
+
+public class Character extends GameObject implements Renderable {
+    private double speed;
+    private Rectangle renderer;
+    private Hitbox hitbox;
+
+    public Character(double x, double y, double width, double height, double speed) {
+        super(x, y, width, height);
+        this.hitbox = new Hitbox(x, y, width, height);
+        this.renderer = new Rectangle(x, y, width, height);
+
+        this.speed = speed;
+    }
+
+    @Override
+    public void draw() {
+        this.renderer.resizeRelocate(x, y, width, height);
+    }
+
+    @Override
+    public void update() {
+        // TODO: bouger si on doit bouger
+    }
+}
