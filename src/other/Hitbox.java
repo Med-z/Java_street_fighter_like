069c5c33e8ee342c35a3b4a9c3c0abf55d38a4d9
Cedric Buchmuller <cedric.buchmuller@etudiant.univ-lr.fr>
2020-06-02 11:21:43 +0200

    méthode pour récupérer le rectangle et les bounds d'une hitbox

--- src/other/Hitbox.java
+++ src/other/Hitbox.java
@@ -1,5 +1,6 @@
 package other;
 
+import javafx.geometry.Bounds;
 import javafx.scene.shape.Rectangle;
 
 public class Hitbox {
@@ -7,6 +8,14 @@ public class Hitbox {
 
     public Hitbox(double x, double y, double width, double height) {
         rectangle = new Rectangle(x, y, width, height);
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public Bounds getBounds() {
+        return rectangle.getLayoutBounds();
     }
 
 
