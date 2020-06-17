4ecd4674244e97ff1b0e3d62f3c26f7bc2d4fcd2
raphael derouelle <raphael.derouelle@etudiant.univ-lr.fr>
2020-06-17 12:49:33 +0200

    Ajout de commentaires

--- src/streetfighter/Main.java
+++ src/streetfighter/Main.java
@@ -50,62 +50,65 @@ public class Main extends Application {
     }
     // startMenu : lancer le menu
     public void startMenu(AnchorPane root) {
+        // Fond du Menu
         final Image menuBackgroundImageSC = new Image("streetfighter/Background/SplashScreenBackground.gif");
         Background background = new Background(0,0, WIDTH, HEIGHT, menuBackgroundImageSC);
         root.getChildren().add(background.renderer);
         
+        // Image Texte du menu
         final Image menuImage = new Image("streetfighter/Menu/Menu.png"); 
         ImageView menuImageView = new ImageView(menuImage);
         AnchorPane.setTopAnchor(menuImageView, 10.0); 
         AnchorPane.setLeftAnchor(menuImageView, 653-(menuImage.getWidth())/2); 
         root.getChildren().add(menuImageView);
         
+        // Image Texte play orange
         final Image playImageO = new Image("streetfighter/Menu/PlayOrange.png"); 
         ImageView playImageViewO = new ImageView(playImageO);
         AnchorPane.setTopAnchor(playImageViewO, 200.0); 
         AnchorPane.setLeftAnchor(playImageViewO, 653-(playImageO.getWidth())/2);
         root.getChildren().add(playImageViewO);
-        
+        // Image Texte play jaune
         final Image playImageY = new Image("streetfighter/Menu/PlayYellow.png"); 
     
-        playImageViewO.setOnMouseEntered(e-> {
-                playImageViewO.setImage(playImageY);
-            }
-        );
-        playImageViewO.setOnMouseExited(e-> {
-                playImageViewO.setImage(playImageO);
-            }
-        );
+        // Switch de couleur lorque l'on passe la souris sur l'image
+        playImageViewO.setOnMouseEntered(e->    { playImageViewO.setImage(playImageY); }  );
+        playImageViewO.setOnMouseExited(e->     { playImageViewO.setImage(playImageO); }  );
+        // Lorsque l'on clique sur play, le combat commence
         playImageViewO.setOnMouseClicked(e-> {
-                root.getChildren().removeAll(root.getChildren()); // Retire tous les éléments de la collection, peu importe l'allure du splash-screen
+                root.getChildren().removeAll(root.getChildren()); 
                 startGame(root);
-            }
-        );
+            });
         
+        // Image Texte Music OFF orange
         final Image musicOFFOImage = new Image("streetfighter/Menu/MusicOFFO.png", 194.5, 46.5, false, false);
         ImageView musicOFFOImageView = new ImageView(musicOFFOImage);
         AnchorPane.setTopAnchor(musicOFFOImageView, 500.0); 
         AnchorPane.setLeftAnchor(musicOFFOImageView, (653-(musicOFFOImage.getWidth())/2)-150); 
         root.getChildren().add(musicOFFOImageView);
-        
+        // Image Texte Music OFF jaune
         final Image musicOFFYImage = new Image("streetfighter/Menu/MusicOFFY.png", 194.5, 46.5, false, false);
         
-        musicOFFOImageView.setOnMouseEntered(e-> { musicOFFOImageView.setImage(musicOFFYImage);});
-        musicOFFOImageView.setOnMouseExited(e-> { musicOFFOImageView.setImage(musicOFFOImage);});
+        // Switch de couleur lorque l'on passe la souris sur l'image
+        musicOFFOImageView.setOnMouseEntered(e->    { musicOFFOImageView.setImage(musicOFFYImage);} );
+        musicOFFOImageView.setOnMouseExited(e->     { musicOFFOImageView.setImage(musicOFFOImage);} );
+        // Lorsque l'on clique sur Music OFF, la musique se met en pause
         musicOFFOImageView.setOnMouseClicked(e-> {
                 music.getMediaPlayer().pause();
             });
-        
+        // Image Texte Music ON orange
         final Image musicONOImage = new Image("streetfighter/Menu/MusicONO.png", 194.5, 46.5, false, false);
         ImageView musicONOImageView = new ImageView(musicONOImage);
         AnchorPane.setTopAnchor(musicONOImageView, 500.0); 
         AnchorPane.setLeftAnchor(musicONOImageView, (653-(musicONOImage.getWidth())/2)+150); 
         root.getChildren().add(musicONOImageView);
-      
+        // Image Texte Music ON jaune
         final Image musicONYImage = new Image("streetfighter/Menu/MusicONY.png", 194.5, 46.5, false, false);
         
-        musicONOImageView.setOnMouseEntered(e-> { musicONOImageView.setImage(musicONYImage);});
-        musicONOImageView.setOnMouseExited(e-> { musicONOImageView.setImage(musicONOImage);});
+        // Switch de couleur lorque l'on passe la souris sur l'image
+        musicONOImageView.setOnMouseEntered(e->     { musicONOImageView.setImage(musicONYImage);} );
+        musicONOImageView.setOnMouseExited(e->      { musicONOImageView.setImage(musicONOImage);} );
+        // Lorsque l'on clique sur Music ON, la musique se met en play
         musicONOImageView.setOnMouseClicked(e-> {
                 music.getMediaPlayer().play();
             });
