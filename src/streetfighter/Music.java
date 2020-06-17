30ee54073e37845553bf79b5828bfc3617b9ff1a
mateo pontoizeau <mateo.pontoizeau@etudiant.univ-lr.fr>
2020-06-17 10:48:49 +0100

    Ajout de la classe Music et du dossier contenant les musiques

---
+++ src/streetfighter/Music.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streetfighter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+/**
+ *
+ * @author mateo
+ */
+public class Music
+{
+    private static MediaPlayer mediaPlayer;
+
+    public MediaPlayer playMusic()
+    {
+        Media song1 = new Media(new File("src\\streetfighter\\Music").toURI().toString());
+        Media song2 = new Media(new File("src\\streetfighter\\Music").toURI().toString());
+        Media song3 = new Media(new File("src\\streetfighter\\Music").toURI().toString());
+        Media song4 = new Media(new File("src\\streetfighter\\Music").toURI().toString());
+        Media song5 = new Media(new File("src\\streetfighter\\Music").toURI().toString());
+
+        List<Media> playlist = new ArrayList<>();
+        playlist.add(song1);
+        playlist.add(song2);
+        playlist.add(song3);
+        playlist.add(song4);
+        playlist.add(song5);
+
+        mediaPlayer = new MediaPlayer(playlist.get((int)(Math.random()*4)+1));
+
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setVolume(25);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+
+        return mediaPlayer;
+    }
+}
